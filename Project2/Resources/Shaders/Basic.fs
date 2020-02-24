e1d9aaa0b0384873c48de173dbb3f004f7be636a
andrew.bar8456 <andrew.bar8456@1814C010>
2020-02-24 16:41:51 +1300

    Week - Three

--- Project2/Resources/Shaders/Basic.fs
+++ Project2/Resources/Shaders/Basic.fs
@@ -1,13 +1,14 @@
 #version 450 core
 
 in vec3 fragColor;
+in vec2 fragTexCoord;
 
 out vec4 color;
 
 uniform float currentTime;
+uniform sampler3D tex;
 
 void main()
 {
-	vec3 colorTemp = fragColor * abs(sin(currentTime));
-	color = vec4(colorTemp, 1.0f);
+	color = texture(tex, fragTexCoord);
 }
--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,20 +1,27 @@
 #include <glew.h>
 #include <freeglut.h>
+#include <SOIL.h>
 #include <iostream>
 
 #include "ShaderLoader.h"
 
 
 GLfloat currentTime;
-
+GLuint texture;
 GLint program;
 GLfloat vertices[]{
-	// Position				// Color
-	-0.5f, -0.5f,  0.0f,	1.0f, 0.0f, 0.0f,	// Left
-	 0.5f, -0.5f,  0.0f,	0.0f, 1.0f, 0.0f,	// Right
-	 0.0f,  0.5f, -0.5f,	0.0f, 0.0f, 1.0f,	// Top
+	// Position				// Color			// Texture Coords
+	-0.5f, -0.5f,  0.0f,	1.0f, 0.0f, 0.0f,	0.0f, 0.0f,	// Top - Left
+	 0.5f, -0.5f,  0.0f,	0.0f, 1.0f, 0.0f,	0.0f, 1.0f,	// Bot - Left
+	 0.0f,  0.5f, -0.5f,	0.0f, 0.0f, 1.0f,	1.0f, 1.0f,	// Bot - Right
+	 0.5f,  0.5f,  0.0f,	0.0f, 0.0f, 1.0f,	1.0f, 0.0f,	// Top - Right
+};
+GLuint indices[] = {
+	0, 1, 2,	// First Triangle
+	0, 2, 3,	// Second Triangle
 };
 GLuint VBO;
+GLuint EBO;
 GLuint VAO;
 
 
@@ -35,6 +42,11 @@ void Render()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glUseProgram(program);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE, texture);
+	glUniform1i(glGetUniformLocation(program, "tex"), 0);
+
 	glBindVertexArray(VAO);		// Bind VAO
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -68,11 +80,19 @@ int main(int argc, char** argv)
 	// Sets the clear colour
 	glClearColor(1.0, 0.0, 0.0, 1.0); // Sets to Red
 
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
+	glEnable(GL_CULL_FACE);
+
 	program = ShaderLoader::CreateProgram(	"Resources/Shaders/Basic.vs",
 											"Resources/Shaders/Basic.fs");
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -83,7 +103,7 @@ int main(int argc, char** argv)
 		3,
 		GL_FLOAT,
 		GL_FALSE,
-		6 * sizeof(GLfloat),	// Stride of the single vertex (pos + color)
+		8 * sizeof(GLfloat),	// Stride of the single vertex (pos + color)
 		(GLvoid*)0);			// Offset from beginning of Vertex
 	glEnableVertexAttribArray(0);
 
@@ -92,9 +112,29 @@ int main(int argc, char** argv)
 		3,
 		GL_FLOAT,
 		GL_FALSE,
-		6 * sizeof(GLfloat),				// Stride of the single vertex (pos + color)
+		8 * sizeof(GLfloat),				// Stride of the single vertex (pos + color)
 		(GLvoid*)(3 * sizeof(GLfloat)));	// Offset from beginning of Vertex
 	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(
+		2,
+		2,
+		GL_FLOAT,
+		GL_FALSE,
+		8 * sizeof(GLfloat),
+		(GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	int width, height;
+	unsigned char* image = SOIL_load_image("Resources/Texture/catz.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
 
 	// Register callbacks
 	glutDisplayFunc(Render);
